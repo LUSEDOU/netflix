c96604c5560cdcf60858b7f277abdf5c489c4e27
LUSEDOU <dolorier_uribe@hotmail.com>
2022-06-27 09:25:53 -0500

    create packages user and authentication repositories

---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// Dart packages which manages the authentication domain.
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
---
+++ packages/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,7 @@
+/// {@template user_repository}
+/// Dart packages which manages the user domain.
+/// {@endtemplate}
+class UserRepository {
+  /// {@macro user_repository}
+  const UserRepository();
+}
