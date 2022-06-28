f7c404f88ae2a3bb76dd4c46fd26eb83a658885b
LUSEDOU <dolorier_uribe@hotmail.com>
2022-06-27 20:51:02 -0500

    add cache and implement it in user repository

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:cache/cache.dart';
+import 'package:meta/meta.dart';
 import 'package:user_repository/user_repository.dart';
 import 'package:uuid/uuid.dart';
 
@@ -8,14 +10,35 @@ import 'package:uuid/uuid.dart';
 /// {@endtemplate}
 class UserRepository {
   /// {@macro user_repository}
-  User? _user;
+  UserRepository({
+    CacheClient? cache,
+  }) : _cache = cache ?? CacheClient();
 
+  final CacheClient _cache;
+
+  /// User cache key.
+  /// Should only be used for testing purposes.
+  @visibleForTesting
+  static const userCacheKey = '__user_cache_key__';
+  
+  /// Returns the current cached user.
+  /// Defaults to [User.empty] if there is no cached user.
+  User get currentUser {
+    return _cache.read<User>(key: userCacheKey) ?? User.empty;
+  }
+
+  /// user
+  User _user = User.empty;
+  
   /// Get and user and give it and id random
-  Future<User?> getUser() async {
-    if (_user != null) return _user;
+  Future<User> get user async {
+    if (_user.isNotEmpty) return _user;
     return Future.delayed(
       const Duration(milliseconds: 300),
-      () => _user = User(id: const Uuid().v4()),
+      () { 
+        _cache.write(key: userCacheKey, value: _user);
+        return _user = User(id: const Uuid().v4());
+      }
     );
   }
 }
